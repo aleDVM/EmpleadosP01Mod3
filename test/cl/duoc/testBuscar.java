1a9c6ecaa543f9776d220ba31c69f835e4aa5a30
aleDVM <pellahuenco@gmail.com>
2019-09-27 17:06:04 -0300

    Version Final

--- test/cl/duoc/testBuscar.java
+++ test/cl/duoc/testBuscar.java
@@ -14,8 +14,8 @@ public class testBuscar {
         System.out.println("Buscar");
         String rut = "55555555";
         BussEmpleado instance = new BussEmpleado();
-        Empleado esperado = new Empleado("55555555", "Karin", 19, 4);
-        Empleado obtenido = instance.Buscar(rut);
+        String esperado = rut;
+        String obtenido = instance.Buscar(rut).getRut();
         assertEquals(esperado, obtenido);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
